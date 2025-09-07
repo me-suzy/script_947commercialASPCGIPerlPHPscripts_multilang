7ea243fea2ad09dcd2504b59c001f597dddc35a4
me-suzy <me.suzana@gmail.com>
2025-09-07 08:47:57 +0300

    Add phpauction-2.1/admin/footer.php

---
+++ phpauction-2.1/admin/footer.php
@@ -0,0 +1,36 @@
+<?
+
+/*
+
+   Copyright (c), 1999, 2000, 2001 - phpauction.org                  
+   
+   This program is free software; you can redistribute it and/or modify 
+   it under the terms of the GNU General Public License as published by 
+   the Free Software Foundation (version 2 or later).                                  
+                                                                        
+   This program is distributed in the hope that it will be useful,      
+   but WITHOUT ANY WARRANTY; without even the implied warranty of       
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+   GNU General Public License for more details.                         
+                                                                        
+   You should have received a copy of the GNU General Public License    
+   along with this program; if not, write to the Free Software          
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   
+*/
+
+?>
+
+<TABLE WIDTH="740" BORDER=0 ALIGN=LEFT>
+<TR><TD>
+<CENTER>
+				
+				<FONT FACE=Verdana,Arial SIZE=1 COLOR="#999999">
+				<? print $MSG_260; ?>
+				</FONT>
+</CENTER>
+</TD></TR>
+</TABLE>
+
+</BODY>
+</HTML>

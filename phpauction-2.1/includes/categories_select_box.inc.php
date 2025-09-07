65b50f343d337ca98750ab54dbbcf56ce2326bcc
me-suzy <me.suzana@gmail.com>
2025-09-07 08:50:07 +0300

    Add phpauction-2.1/includes/categories_select_box.inc.php

---
+++ phpauction-2.1/includes/categories_select_box.inc.php
@@ -0,0 +1,22 @@
+<SELECT NAME="id">
+	<OPTION VALUE="0"></OPTION>
+	<OPTION VALUE="1">Art & Antiques</OPTION>
+	<OPTION VALUE="29">Books</OPTION>
+	<OPTION VALUE="71">Clothing & Accessories</OPTION>
+	<OPTION VALUE="75">Coins & Stamps</OPTION>
+	<OPTION VALUE="78">Collectibles</OPTION>
+	<OPTION VALUE="116">Comics, Cards & Science Fiction</OPTION>
+	<OPTION VALUE="125">Computers & Software</OPTION>
+	<OPTION VALUE="130">Electronics & Photography</OPTION>
+	<OPTION VALUE="136">Gemstones & Jewelry</OPTION>
+	<OPTION VALUE="151">Home & Garden</OPTION>
+	<OPTION VALUE="161">Movies & Video</OPTION>
+	<OPTION VALUE="166">Music</OPTION>
+	<OPTION VALUE="173">Office & Business</OPTION>
+	<OPTION VALUE="178">Other Goods & Services</OPTION>
+	<OPTION VALUE="186">Sports & Recreation</OPTION>
+	<OPTION VALUE="190">Toys & Games</OPTION>
+	<OPTION VALUE="222">Video Games</OPTION>
+	<OPTION VALUE=""></OPTION>
+	<OPTION VALUE="0">All categories</OPTION>
+</SELECT>

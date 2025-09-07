160652f0bfcdb415862a9e83aa5cb5acf96b4ce3
me-suzy <me.suzana@gmail.com>
2025-09-07 09:05:16 +0300

    Add VBB_mit_23_Hacks_Portal_inkl/Board/includes/blocks/text.php

---
+++ VBB_mit_23_Hacks_Portal_inkl/Board/includes/blocks/text.php
@@ -0,0 +1,25 @@
+<?php
+$blocks_modules[text] = array(
+	'title' => 'Plain Text',
+	'func_display' => 'blocks_text_block',
+	'func_add' => '',
+	'func_update' => '',
+	'text_type' => 'Text',
+	'text_type_long' => 'Plain Text',
+	'text_content' => 'Plain Text',
+	'support_nukecode' => true,
+	'allow_create' => true,
+	'allow_delete' => true,
+	'form_url' => false,
+	'form_content' => true,
+	'form_refresh' => false,
+	'show_preview' => true,
+    'has_templates' => false
+    );
+function blocks_text_block($row) {
+	global $block_sidetemplate;
+	$block_title = $row[title];
+    $block_content = $row[content];
+	eval("dooutput(\"".gettemplate($block_sidetemplate)."\");");
+}
+?>

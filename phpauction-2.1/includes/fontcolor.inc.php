2f00b3cc2e0a22e54c517d01713e68a151ec0e83
me-suzy <me.suzana@gmail.com>
2025-09-07 08:50:26 +0300

    Add phpauction-2.1/includes/fontcolor.inc.php

---
+++ phpauction-2.1/includes/fontcolor.inc.php
@@ -0,0 +1,34 @@
+<?
+	/*
+		NOTE: change the following array as you whish but
+		be sure to always have a 1 chars long index.
+		Example:
+			"a" => "#333333"  <--- This is valid 
+			"ab" => "#333333" <-- This is NOT valid
+	*/
+	$FONTCOLOR = array("a" => "#000000",
+				       "b" => "#999999",
+				       "c" => "#EEEEEE",
+				       "d" => "#FFFFFF",
+				       "e" => "#CC0066",
+				       "f" => "#FF0033",
+				       "g" => "#FF99CC",
+				       "h" => "#FFEAFF",
+				       "i" => "#330099",
+				       "l" => "#3300FF",
+				       "m" => "#33CCFF",
+				       "n" => "#E4EDF1",
+				       "o" => "##006633",
+				       "p" => "#009933",
+				       "q" => "#00FF33",
+				       "r" => "#DCF2DB",
+				       "s" => "#CC00CC",
+				       "t" => "#FF66FF",
+				       "u" => "#FFCCFF",
+				       "v" => "#FCE9FC",
+				       "z" => "#993300",
+				       "x" => "#996600",
+				       "w" => "#D6C9A3",
+				       "y" => "##EEE8DB"
+				  );
+?>

5a3a62b2d2467916f982bddcdb88d85e2d398b39
me-suzy <me.suzana@gmail.com>
2025-09-07 08:50:39 +0300

    Add phpauction-2.1/includes/mail_endauction_winner.inc.php

---
+++ phpauction-2.1/includes/mail_endauction_winner.inc.php
@@ -0,0 +1,64 @@
+#
+# --Send winner email address to seller
+# 
+# 			This file contains the message your customers
+# 			will receive when someone sends them an auction.
+#			Lines starting with # will be skipped.
+#			Blank lines will be maintained.
+#
+#			Change the message below as needed considering the 
+#			following tags to reflect your customer's personal data:
+#
+#        --------------------------------------------------------
+#			TAG SYNTAX				EFFECT
+#        --------------------------------------------------------
+#
+#			<#s_name#>              Seller Name
+#			<#s_nick#>              Seller Nickname
+#			<#s_email#>             Seller email
+#			<#s_address#>           Seller Address
+#			<#s_city#>              Seller City
+#			<#s_prov#>              Seller State/Province
+#			<#s_country#>           Seller Country
+#			<#s_zip#>               Seller Zip Code
+#			<#s_phone#>             Seller Phone
+#			<#i_title#>             auction item title 
+#			<#i_description#>       auction item description 
+#			<#i_url#>               URL to view auction 
+#			<#i_ends#>              Auction End date/time
+#			<#i_qty#>              Auction End date/time
+#           <#w_report#>            Winner report/list
+#           <#c_sitename#>          Auction Site Name
+#           <#c_siteurl#>           main URL of auction site
+#           <#c_adminemail#>        email address of Auction site webmaster
+#        --------------------------------------------------------
+#
+#			USAGE:
+#			Insert the above tags in the text of your message			
+#			where you want each value to appear.			
+#			Modify the message to reflect your needs.
+#			Change [...] with to your correct data.
+#
+# 
+#
+
+Dear <#s_name#>,
+
+The auction you created at <#c_sitename#> has closed.  Winner information is below:
+
+Title: <#i_title#>
+Item: <#i_description#> 
+Quantity: <#i_qty#>
+End Date: <#i_ends#>
+Bids: <#i_currentbid#>
+
+Please contact the winner(s) below:
+Winner(s) List:
+ <#w_report#>
+
+An email has been sent to the winner(s) with your email address.
+
+If you have received this message in error, please reply to this email,
+write to <#c_adminemail#>, or visit <#c_sitename#> at <#c_siteurl#>.
+
+

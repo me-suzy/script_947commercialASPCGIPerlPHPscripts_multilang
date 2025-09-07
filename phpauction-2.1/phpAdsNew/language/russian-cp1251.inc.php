3dfebc728e25ebbd3abb20d24948c46d1de50419
me-suzy <me.suzana@gmail.com>
2025-09-07 08:57:56 +0300

    Add phpauction-2.1/phpAdsNew/language/russian-cp1251.inc.php

---
+++ phpauction-2.1/phpAdsNew/language/russian-cp1251.inc.php
@@ -0,0 +1,258 @@
+<?php // $Revision: 1.1.1.1 $
+
+/************************************************************************/
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
+/*                                                                      */
+/* Copyright (c) 2001 by the phpAdsNew developers                       */
+/* http://sourceforge.net/projects/phpadsnew                            */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+// Set character-set
+$GLOBALS['phpAds_CharSet'] = "koi8-r";
+
+
+// Set translation strings
+$GLOBALS['strHome'] = "Ãëàâíàÿ ñòðàíèöà";
+$GLOBALS['date_format'] = "%d/%m/%Y";
+$GLOBALS['time_format'] = "%H:%i:%S";
+$GLOBALS['strMySQLError'] = "MySQL-Error:";
+$GLOBALS['strAdminstration'] = "Àäìèíèñòðèðîâàíèå";
+$GLOBALS['strAddClient'] = "Äîáàâèòü êëèåíòà";
+$GLOBALS['strModifyClient'] = "Èçìåíèòü êëèåíòà";
+$GLOBALS['strDeleteClient'] = "Óäàëèòü êëèåíòà";
+$GLOBALS['strViewClientStats'] = "Ïîñìîòðåòü ñòàòèñòèêó êëèåíòà";
+$GLOBALS['strClientName'] = "Êëèåíò";
+$GLOBALS['strContact'] = "Êîíòàêò";
+$GLOBALS['strEMail'] = "EMail";
+$GLOBALS['strViews'] = "Ïîêàçîâ";
+$GLOBALS['strClicks'] = "Êëèêîâ";
+$GLOBALS['strTotalViews'] = "Âñåãî ïîêàçîâ";
+$GLOBALS['strTotalClicks'] = "Âñåãî êëèêîâ";
+$GLOBALS['strCTR'] = "Click-Through Ratio";
+$GLOBALS['strTotalClients'] = "Âñåãî êëèåíòîâ";
+$GLOBALS['strActiveClients'] = "Àêòèâíûõ êëèåíòîâ";
+$GLOBALS['strActiveBanners'] = "Àêòèâíûõ áàííåðîâ";
+$GLOBALS['strLogout'] = "Âûõîä";
+$GLOBALS['strCreditStats'] = "Ñòàòèñòèêà êðåäèòîâ";
+$GLOBALS['strViewCredits'] = "Êðåäèòû ïî ïîêàçàì";   
+$GLOBALS['strClickCredits'] = "Êðåäèòû ïî êëèêàì";
+$GLOBALS['strPrevious'] = "Ïðåäûäóùèé";
+$GLOBALS['strNext'] = "Ñëåäóþùèé";
+$GLOBALS['strNone'] = "Íåò";
+$GLOBALS['strViewsPurchased'] = "Êóïëåíî ïîêàçîâ";
+$GLOBALS['strClicksPurchased'] = "Êóïëåíî êëèêîâ";
+$GLOBALS['strDaysPurchased'] = "Êóïëåíî äíåé";
+$GLOBALS['strHTML'] = "HTML";
+$GLOBALS['strAddSep'] = "Çàïîëíÿòü ïîëÿ âûøå ÈËÈ ïîëÿ íèæå!";
+$GLOBALS['strTextBelow'] = "Òåêñò ïîä êàðòèíêîé";
+$GLOBALS['strSubmit'] = "Çàãðóçèòü áàííåð";
+$GLOBALS['strUsername'] = "Ëîãèí";
+$GLOBALS['strPassword'] = "Ïàðîëü";
+$GLOBALS['strBannerAdmin'] = "Àäìèíèñòðèðóåì áàííåðû äëÿ";
+$GLOBALS['strBannerAdminAcl'] = "Êîíòðîëü òàðãåòèíãà äëÿ";
+$GLOBALS['strNoBanners'] = "Íåò áàííåðîâ";
+$GLOBALS['strBanner'] = "Áàííåð";
+$GLOBALS['strCurrentBanner'] = "Òåêóùèé áàííåð";
+$GLOBALS['strDelete'] = "Óäàëèòü";
+$GLOBALS['strAddBanner'] = "Äîáàâèòü íîâûé áàííåð";
+$GLOBALS['strModifyBanner'] = "Èçìåíèòü áàííåð";
+$GLOBALS['strModifyBannerAcl'] = "Èçìåíèòü ïàðàìåòðû òàðãåòèíãà";
+$GLOBALS['strURL'] = "URL (ñ http://)";
+$GLOBALS['strKeyword'] = "êëþ÷åâîå ñëîâî (ëàòèíèöåé è öèôðàìè)";
+$GLOBALS['strWeight'] = "Âåñ";
+$GLOBALS['strAlt'] = "Alt-Tåêñò";
+$GLOBALS['strUsername'] = "Ëîãèí";
+$GLOBALS['strPassword'] = "Ïàðîëü";
+$GLOBALS['strAccessDenied'] = "Äîñòóï çàïðåù¸í";
+$GLOBALS['strPasswordWrong'] = "Ïàðîëü óêàçàí íåâåðíî";
+$GLOBALS['strNotAdmin'] = "Âåðîÿòíî, ó âàñ íåò ïðàâ äîñòóïà";
+$GLOBALS['strClientAdded'] = "Êëèåíò äîáàâëåí.";
+$GLOBALS['strClientModified'] = "Êëèåíò èçìåí¸í.";
+$GLOBALS['strClientDeleted'] = "Êëèåíò óäàëåí.";
+$GLOBALS['strBannerAdmin'] = "Àäìèíèñòðèðîâàíèå áàííåðîâ";
+$GLOBALS['strBannerAdded'] = "Áàííåð äîáàâëåí.";
+$GLOBALS['strBannerModified'] = "Áàííåð èçìåí¸í.";
+$GLOBALS['strBannerDeleted'] = "Áàííåð óäàë¸í";
+$GLOBALS['strBannerChanged'] = "Áàííåð èçìåí¸í";
+$GLOBALS['strStats'] = "Ñòàòèñòèêà";
+$GLOBALS['strDailyStats'] = "Ñòàòèñòèêà ïî äíÿì";
+$GLOBALS['strDetailStats'] = "Äåòàëüíàÿ ñòàòèñòèêà";
+$GLOBALS['strCreditStats'] = "Ñòàòèñòèêà ïî êðåäèòàì";
+$GLOBALS['strActive'] = "àêòèâåí";
+$GLOBALS['strActivate'] = "Àêòèâèðîâàòü";
+$GLOBALS['strDeActivate'] = "Äåàêòèâèðîâàòü";
+$GLOBALS['strAuthentification'] = "Äîñòóï";
+$GLOBALS['strGo'] = "Ïîø¸ë!";
+$GLOBALS['strLinkedTo'] = "ññûëêà";
+$GLOBALS['strBannerID'] = "Banner-ID";
+$GLOBALS['strClientID'] = "Client ID";
+$GLOBALS['strMailSubject'] = "Îò÷¸ò î ðåêëàìå";
+$GLOBALS['strMailSubjectDeleted'] = "Äåàêòèâèðîâàííûå áàííåðû";
+$GLOBALS['strMailHeader'] = "Äîðîãîé {contact},\n";
+$GLOBALS['strMailBannerStats'] = "Çäåñü âû âèäèòå ñòàòèñòèêó êëèåíòà {clientname}:";
+$GLOBALS['strMailFooter'] = "ñ íàèëó÷øèìè ïîæåëàíèÿìè,\n   {adminfullname}";
+$GLOBALS['strLogMailSent'] = "[phpAds] Ñòàòèñòèêà óñïåøíî îòïðàâëåíà.";
+$GLOBALS['strLogErrorClients'] = "[phpAds] îøèáêà äîñòóïà ê áàçå äàííûõ èíôîðìàöèè î êëèåíòàõ.";
+$GLOBALS['strLogErrorBanners'] = "[phpAds] Îøèáêà äîñòóïà ê ÁÄ áàííåðîâ.";
+$GLOBALS['strLogErrorViews'] = "[phpAds] Îøèáêà äîñòóïà ê ÁÄ ïîêàçîâ.";
+$GLOBALS['strLogErrorClicks'] = "[phpAds] Îøèáêà äîñòóïà ê ÁÄ êëèêîâ.";
+$GLOBALS['strLogErrorDisactivate'] = "[phpAds] Îøèáêà äåàêòèâàöèè áàííåðà.";
+$GLOBALS['strRatio'] = "Ðåéòèíã êëèêàáåëüíîñòè";
+$GLOBALS['strChooseBanner'] = "Âûáåðèòå òèï áàííåðà.";
+$GLOBALS['strMySQLBanner'] = "Áàííåð ñ âàøåãî êîìïüþòåðà ïîìåùàåòñÿ íà ñåðâåð";
+$GLOBALS['strWebBanner'] = "Banner stored on the Webserver";
+$GLOBALS['strURLBanner'] = "Áàííåð ëåæèò ãäå-òî â Èíòåðíåò";
+$GLOBALS['strHTMLBanner'] = "Òåêñòîâûé áàííåð";
+$GLOBALS['strNewBannerFile'] = "Ôàéë áàííåðà íà äèñêå";
+$GLOBALS['strNewBannerURL'] = "URL áàííåðà (ñ http://)";
+$GLOBALS['strWidth'] = "øèðèíà";
+$GLOBALS['strHeight'] = "âûñîòà";
+$GLOBALS['strTotalViews7Days'] = "Âñåãî ïîêàçîâ çà íåäåëþ";
+$GLOBALS['strTotalClicks7Days'] = "Âñåãî êëèêîâ çà íåäåëþ";
+$GLOBALS['strAvgViews7Days'] = "Â ñðåäíåì ïîêàçîâ çà íåäåëþ";
+$GLOBALS['strAvgClicks7Days'] = "Â ñðåäíåì êëèêîâ çà íåäåëþ";
+$GLOBALS['strTopTenHosts'] = "Top ten àäðåñîâ ïðîñìàòðèâàâøèõ áàííåðû";
+$GLOBALS['strConfirmDeleteClient'] = "Âû äåéñòâèòåëüíî õîòèòå óäàëèòü ýòîãî êëèåíòà?";
+$GLOBALS['strClientIP'] = "IP Êëèåíòà";
+$GLOBALS['strUserAgent'] = "regexp ñòðîêè User-agent";
+$GLOBALS['strWeekDay'] = "Äåíü íåäåëè (0 - 6) ((ñ âîñêðåñåíüÿ))";
+$GLOBALS['strDomain'] = "Äîìåí (áåç òî÷êè â íà÷àëå)";
+$GLOBALS['strSource'] = "Èñòî÷íèê";
+$GLOBALS['strTime'] = "Âðåìÿ";
+$GLOBALS['strAllow'] = "Äîñòóï îòêðûò äëÿ";
+$GLOBALS['strDeny'] = "Äîñòóï çàêðûò äëÿ";
+$GLOBALS['strConfirmResetStats'] = "Âû äåéñòâèòåëüíî õîòèòå îáíóëèòü ñòàòèñòèêó äëÿ ýòîãî êëèåíòà ?";
+$GLOBALS['strExpiration'] = "Expiration";
+$GLOBALS['strNoExpiration'] = "No expiration date set";
+$GLOBALS['strDaysLeft'] = "Îñòàëîñü äíåé";
+$GLOBALS['strEstimated'] = "Estimated expiration";
+$GLOBALS['strConfirm'] = "Âû óâåðåíû ?";
+$GLOBALS['strBannerNoStats'] = "Íåò ñòàòèñòèêè äëÿ ýòîãî áàííåðà!";
+$GLOBALS['strWeek'] = "Íåäåëÿ";
+$GLOBALS['strWeeklyStats'] = "Åæåíåäåëüíàÿ ñòàòèñòèêà";
+$GLOBALS['strWeekDay'] = "Äåíü íåäåëè";
+$GLOBALS['strDate'] = "Äàòà";
+$GLOBALS['strCTRShort'] = "CTR";
+$GLOBALS['strDayShortCuts'] = array("Âñ","Ïí","Âò","Ñð","×ò","Ïò","Ñá");
+$GLOBALS['strShowWeeks'] = "Ìàêñ. ÷èñëî ïîêàçûâàåìûõ íåäåëü";
+$GLOBALS['strAll'] = "âñå";
+$GLOBALS['strAvg'] = "Ñðåäíåå";
+$GLOBALS['strHourly'] = "Ïðîñìîòðîâ/Êëèêîâ ïî ÷àñàì";
+$GLOBALS['strTotal'] = "Âñåãî";
+$GLOBALS['strUnlimited'] = "Íå îãðàíè÷åíî";
+$GLOBALS['strSave'] = "Ñîõðàíèòü";
+$GLOBALS['strUp'] = "Ââåðõ";
+$GLOBALS['strDown'] = "Âíèç";
+$GLOBALS['strSaved'] = "áûë ñîõðàíåí!";
+$GLOBALS['strDeleted'] = "áûë óäàëåí!";  
+$GLOBALS['strMovedUp'] = "áûë ïåðåìåùåí âûøå";
+$GLOBALS['strMovedDown'] = "áûë ïåðåìåùåí íèæå";
+$GLOBALS['strUpdated'] = "áûë îáíîâëåí";
+$GLOBALS['strACL'] = "ACL";
+$GLOBALS['strNoMoveUp'] = "Íå ìîãó ïåðåìåñòèòü ïåðâûé ðÿä âûøå";
+$GLOBALS['strACLAdd'] = "Äîáàâèòü íîâûé ".$GLOBALS["strACL"];
+$GLOBALS['strACLExist'] = "Ñóùåñòâóþùèé ".$GLOBALS["strACL"].":";
+$GLOBALS['strLogin'] = "Login";
+$GLOBALS['strPreferences'] = "Ïðåäïî÷òåíèÿ";
+$GLOBALS['strAllowClientModifyInfo'] = "Ðàçðåøèòü ýòîìó ïîëüçîâàòåëþ ðåäàêòèðîâàòü ñîáñòâåííûå êëèåíòñêèå äàííûå";
+$GLOBALS['strAllowClientModifyBanner'] = "Ðàçðåøèòü ýòîìó ïîëüçîâàòåëþ ìîäèôèöèðîâàòü ñîáñòâåííûå áàííåðû";
+$GLOBALS['strAllowClientAddBanner'] = "Ðàçðåøèòü ýòîìó ïîëüçîâàòåëþ äîáàâëÿòü íîâûå áàííåðû";
+$GLOBALS['strLanguage'] = "ßçûê";
+$GLOBALS['strDefault'] = "Ïî óìîë÷àíèþ";
+$GLOBALS['strErrorViews'] = "Âû äîëæíû ââåñòè ÷èñëî ïîêàçîâ èëè âûáðàòü 'Íå îãðàíè÷åíî' !";
+$GLOBALS['strErrorNegViews'] = "Îòðèöàòåëüíîå ÷èñëî ïîêàçîâ íå ðàçðåøåíî";
+$GLOBALS['strErrorClicks'] =  "Âû äîëæíû ââåñòè ÷èñëî êëèêîâ èëè âûáðàòü 'Íå îãðàíè÷åíî' !";
+$GLOBALS['strErrorNegClicks'] = "Îòðèöàòåëüíîå ÷èñëî êëèêîâ íå ðàçðåøåíî";
+$GLOBALS['strErrorDays'] = "Âû äîëæíû ââåñòè ÷èñëî äíåé èëè âûáðàòü 'Íå îãðàíè÷åíî' !";
+$GLOBALS['strErrorNegDays'] = "Îòðèöàòåëüíîå ÷èñëî äíåé íå ðàçðåøåíî";
+$GLOBALS['strTrackerImage'] = "Tracker image:";
+
+// New strings for version 2
+$GLOBALS['strNavigation'] 				= "Íàâèãàöèÿ";
+$GLOBALS['strShortcuts'] 				= "Shortcuts";
+$GLOBALS['strDescription'] 				= "Îïèñàíèå";
+$GLOBALS['strClients'] 					= "Êëèåíòû";
+$GLOBALS['strID']				 		= "ID";
+$GLOBALS['strOverall'] 					= "Âñåãî";
+$GLOBALS['strTotalBanners'] 			= "Âñåãî áàííåðîâ";
+$GLOBALS['strToday'] 					= "Ñåãîäíÿ";
+$GLOBALS['strThisWeek'] 				= "Çà ýòó íåäåëþ";
+$GLOBALS['strThisMonth'] 				= "Çà ýòîò ìåñÿö";
+$GLOBALS['strBasicInformation'] 		= "Îñíîâíàÿ èíôîðìàöèÿ";
+$GLOBALS['strContractInformation'] 		= "Êîíòðàêòíàÿ èíôîðìàöèÿ";
+$GLOBALS['strLoginInformation'] 		= "Èíôîðìàöèÿ î ëîãèíå";
+$GLOBALS['strPermissions'] 				= "Äîïóñêè";
+$GLOBALS['strGeneralSettings']			= "Îáùèå óñòàíîâêè";
+$GLOBALS['strSaveChanges']		 		= "Ñîõðàíèòü èçìåíåíèÿ";
+$GLOBALS['strCompact']					= "Êîìïàêòíî";
+$GLOBALS['strVerbose']					= "Ïîäðîáíî";
+$GLOBALS['strOrderBy']					= "order by";
+$GLOBALS['strShowAllBanners']	 		= "Ïîêàçàòü âñå áàííåðû";
+$GLOBALS['strShowBannersNoAdClicks']	= "Ïîêàçàòü áàííåðû áåç êëèêîâ";
+$GLOBALS['strShowBannersNoAdViews']		= "Ïîêàçàòü áàííåðû áåç ïðîñìîòðîâ";
+$GLOBALS['strShowAllClients'] 			= "Ïîêàçàòü âñåõ êëèåíòîâ";
+$GLOBALS['strShowClientsActive'] 		= "Ïîêàçàòü êëèåíòîâ ñ àêòèâíûìè áàííåðàìè";
+$GLOBALS['strShowClientsInactive']		= "Ïîêàçàòü êëèåíòîâ ñ íåàêòèâíûìè áàííåðàìè";
+$GLOBALS['strSize']						= "Ðàçìåð";
+
+$GLOBALS['strMonth'] 					= array("ßíâàðü","Ôåâðàëü","Ìàðò","Àïðåëü","Ìàé","Èþíü","Èþëü", "Àâãóñò", "Ñåíòÿáðü", "Îêòÿáðü", "Íîÿáðü", "Äåêàáðü");
+$GLOBALS['strDontExpire']				= "Íå äåàêòèâèðîâàòü ýòîãî êëèåíòà ïî íàñòóïëåíèþ óêàçàííîé äàòû";
+$GLOBALS['strActivateNow'] 				= "Íåìåäëåííî àêòèâèðîâàòü ýòîãî êëèåíòà";
+$GLOBALS['strExpirationDate']			= "Äàòà äåàêòèâàöèè";
+$GLOBALS['strActivationDate']			= "Äàòà àêòèâàöèè";
+
+$GLOBALS['strMailClientDeactivated'] 	= "Âàøè áàííåðû áëè âûêëþ÷åíû, òàê êàê";
+$GLOBALS['strMailNothingLeft'] 			= "Åñëè áû âû õîòåëè ïðîäîëæàòü ðàçìåùàòü ðåêëàìó íà íàøåì ñàéòå, ïîæàëóéñòà, ñâÿæèòåñü ñ íàìè. Ìû áóäåì ðàäû âíîâü ñëûøàòü âàñ.";
+$GLOBALS['strClientDeactivated']		= "Äàííûé êëèåíò â íàñòîÿùåå âðåìÿ äåàêòèâèðîâàí, òàê êàê";
+$GLOBALS['strBeforeActivate']			= "äàòà àêòèâàöèè åùå íå äîñòèãíóòà";
+$GLOBALS['strAfterExpire']				= "áûëà äîñòèãíóòà äàòà äåàêòèâàöèè";
+$GLOBALS['strNoMoreClicks']				= "âñå ïðèîáðåòåííûå êëèêè èñïîëüçîâàíû";
+$GLOBALS['strNoMoreViews']				= "âñå ïðèîáðåòåííûå ïðîñìîòðû èñïîëüçîâàíû";
+
+$GLOBALS['strBanners'] 					= "Áàííåðû";
+$GLOBALS['strCampaigns']				= "Êàìïàíèè";
+$GLOBALS['strCampaign']					= "Êàìïàíèÿ";
+$GLOBALS['strName']						= "Èìÿ";
+$GLOBALS['strBannersWithoutCampaign']	= "Áàííåðû áåç êàìïàíèè";
+$GLOBALS['strMoveToNewCampaign']		= "Ïåðåéòè ê íîâîé êàìïàíèè";
+$GLOBALS['strCreateNewCampaign']		= "Ñîçäàòü íîâóþ êàìïàíèþ";
+$GLOBALS['strEditCampaign']				= "Ðåäàêòèðîâàòü êàìïàíèþ";
+$GLOBALS['strEdit']						= "Ðåäàêòèðîâàòü";
+$GLOBALS['strCreate']					= "Ñîçäàòü";
+$GLOBALS['strUntitled']					= "Áåç íàçâàíèÿ";
+
+$GLOBALS['strTotalCampaigns'] 			= "Âñåãî êàìïàíèé";
+$GLOBALS['strActiveCampaigns'] 			= "Àêòèâíûõ êàìïàíèé";
+
+$GLOBALS['strLinkedTo']					= "ñâÿçàíî ñ";
+$GLOBALS['strNoViewLoggedInInterval']   = "Íå çàðåãèñòðèðîâàíî ïðîñìîòðîâ çà ïîñëåäíèå {interval} äíåé";
+$GLOBALS['strNoClickLoggedInInterval']  = "Íå çàðåãèñòðèðîâàíî êëèêîâ çà ïîñëåäíèå {interval} äíåé";
+$GLOBALS['strSendAdvertisingReport']	= "Ïîñûëàòü ðåêëàìíûé îò÷åò ïî e-mail";
+$GLOBALS['strNoDaysBetweenReports']		= "Êîëè÷åñòâî äíåé ìåæäó îò÷åòàìè";
+$GLOBALS['strSendDeactivationWarning']  = "Ïîñûëàòü ïðåäóïðåæäåíèå, êîãäà êàìïàíèÿ äåàêòèâèðîâàíóåòñÿ";
+
+$GLOBALS['strWarnClientTxt']			= "Êîëè÷åñòâî íàæàòèé èëè ïðîñìîòðîâ äëÿ âàøèõ áàííåðîâ ñêîðî ñòàíåò ìåíüøå {limit}. ";
+$GLOBALS['strViewsClicksLow']			= "Ðåêëàìíûå ïðîñìîòðû/íàæàòèÿ ïîäõîäÿò ê êîíöó";
+
+$GLOBALS['strDays']						= "Äíè";
+$GLOBALS['strHistory']					= "Èñòîðèÿ";
+$GLOBALS['strAverage']					= "Â ñðåäíåì";
+$GLOBALS['strDuplicateClientName']		= "Óêàçàííîå èìÿ ïîëüçîâàòåëÿ óæå ñóùåñòâóåò, ïîæàëóéñòà ââåäèòå äðóãîå èìÿ.";
+$GLOBALS['strAllowClientDisableBanner'] = "Ðàçðåøèòü ýòîìó ïîëüçîâàòåëþ äåàêòèâèðîâàòü åãî áàííåðû";
+$GLOBALS['strAllowClientActivateBanner'] = "Ðàçðåøèòü ýòîìó ïîëüçîâàòåëþ àêòèâèðîâàòü åãî áàííåðû";
+
+$GLOBALS['strGenerateBannercode']		= "Generate Bannercode";
+$GLOBALS['strChooseInvocationType']		= "Please choose the type of banner invocation";
+$GLOBALS['strGenerate']					= "Generate";
+$GLOBALS['strParameters']				= "Parameters";
+$GLOBALS['strUniqueidentifier']			= "Unique identifier";
+$GLOBALS['strFrameSize']				= "Frame size";
+$GLOBALS['strBannercode']				= "Bannercode";
+
+?>

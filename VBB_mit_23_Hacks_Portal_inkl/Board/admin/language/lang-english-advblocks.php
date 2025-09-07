7e795c375c9e76808725e6847cb3667f59fd0f74
me-suzy <me.suzana@gmail.com>
2025-09-07 09:00:47 +0300

    Add VBB_mit_23_Hacks_Portal_inkl/Board/admin/language/lang-english-advblocks.php

---
+++ VBB_mit_23_Hacks_Portal_inkl/Board/admin/language/lang-english-advblocks.php
@@ -0,0 +1,56 @@
+<?php
+/*****************************************************************************
+ * Advanced Blocks System
+ *
+ * Copyright (c) 2001 Patrick Kellum (webmaster@quahog-library.com)
+ * http://quahog-library.com/
+ *
+ * Based in part of the blocks system in PHP-Nuke
+ * Copyright (c) 2001 by Francisco Burzi (fbc@mandrakesoft.com)
+ * http://phpnuke.org/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+define('_AB_ALINKCONTENT','Admin Links');
+define('_AB_BLOCKSADMIN','&quot;Erweiterte Block&quot; Administration');
+define('_AB_BLOCKSLIST','Seiten Blöcke');
+define('_AB_BOTTOM', 'Unten');
+define('_AB_BOTTOMBLOCK', 'Unterer Block');
+define('_AB_CENTER', 'Mitte');
+define('_AB_CENTERBLOCK', 'Mittlerer Block');
+define('_AB_INCLUDECONTENT','Included PHP File');
+define('_AB_LABEL_URL','URL');
+define('_AB_LINKCONTENT','Links');
+define('_AB_NNTPCONTENT','NNTP/Usenet Newsgroup');
+define('_AB_PHPCONTENT','PHP Script');
+define('_AB_RSSCONTENT','RSS/RDF Content');
+define('_AB_TEXTCONTENT','HTML/Text File');
+define('_AB_TOP', 'Oben');
+define('_AB_TOPBLOCK', 'Oberer Block');
+define('_AB_TYPE_ALINK','Admin Link');
+define('_AB_TYPE_ALINK2','Admin Link Liste');
+define('_AB_TYPE_HTML','HTML/Text');
+define('_AB_TYPE_HTML2','HTML/Text Block');
+define('_AB_TYPE_INCLUDE','Include');
+define('_AB_TYPE_INCLUDE2','Include PHP File');
+define('_AB_TYPE_LINK','Link');
+define('_AB_TYPE_LINK2','Link Liste');
+define('_AB_TYPE_NNTP','NNTP');
+define('_AB_TYPE_NNTP2','NNTP/Usenet');
+define('_AB_TYPE_PHP','PHP');
+define('_AB_TYPE_PHP2','PHP Script');
+define('_AB_TYPE_RDF','RDF/RSS');
+define('_AB_TYPE_RDF2','RDF/RSS News');
+?>

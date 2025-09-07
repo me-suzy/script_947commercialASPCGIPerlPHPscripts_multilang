8f61a7c1d4cb2a5e4370c3f80ceba53b96dabc75
me-suzy <me.suzana@gmail.com>
2025-09-07 08:50:49 +0300

    Add phpauction-2.1/includes/passwd.inc.php

---
+++ phpauction-2.1/includes/passwd.inc.php
@@ -0,0 +1,29 @@
+<?
+  
+/*
+
+   Copyright (c), 1999, 2000 - phpauction.org                  
+   
+   This program is free software; you can redistribute it and/or modify 
+   it under the terms of the GNU General Public License as published by 
+   the Free Software Foundation (version 2 or later).                                  
+                                                                        
+   This program is distributed in the hope that it will be useful,      
+   but WITHOUT ANY WARRANTY; without even the implied warranty of       
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+   GNU General Public License for more details.                         
+                                                                        
+   You should have received a copy of the GNU General Public License    
+   along with this program; if not, write to the Free Software          
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   
+*/
+
+//--Set the variables below according to your system configuration
+
+$DbHost     = "localhost"; // The host where the MySQL server resides
+$DbDatabase = "dbname"; // The database you are going to use
+$DbUser     = "username"; // Username
+$DbPassword = "password"; // Password
+
+?>

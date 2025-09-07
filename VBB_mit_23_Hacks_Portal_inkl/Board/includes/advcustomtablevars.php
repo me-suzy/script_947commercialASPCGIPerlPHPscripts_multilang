440be51517e00f83fdfc10674f98acd9cad260a2
me-suzy <me.suzana@gmail.com>
2025-09-07 09:04:27 +0300

    Add VBB_mit_23_Hacks_Portal_inkl/Board/includes/advcustomtablevars.php

---
+++ VBB_mit_23_Hacks_Portal_inkl/Board/includes/advcustomtablevars.php
@@ -0,0 +1,6 @@
+<?php
+$nuketable[advblocks]               = $prefix.'_advblocks';
+$nuketable[advheadlines]            = $prefix.'_advheadlines';
+$nuketable[centerblocks]            = $prefix.'_centerblocks';
+$nuketable[forumblocks]            = $prefix.'_forumblocks';
+?>

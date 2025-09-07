abbb4245583a0d363c4e5e180f24e21396647a17
me-suzy <me.suzana@gmail.com>
2025-09-07 08:46:26 +0300

    Add phpauction-2.1/help_password.php

---
+++ phpauction-2.1/help_password.php
@@ -0,0 +1,57 @@
+<SCRIPT Language=PHP>
+/*
+
+   Copyright (c), 1999, 2000 - phpauction.org                  
+   
+   This program is free software; you can redistribute it and/or modify 
+   it under the terms of the GNU General Public License as published by 
+   the Free Software Foundation (version 2 or later).                                  
+                                                                        
+   This program is distributed in the hope that it will be useful,      
+   but WITHOUT ANY WARRANTY; without even the implied warranty of       
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+   GNU General Public License for more details.                         
+                                                                        
+   You should have received a copy of the GNU General Public License    
+   along with this program; if not, write to the Free Software          
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   
+*/
+
+
+	// Include messages file	
+   require('./includes/messages.inc.php');
+  
+   // Connect to sql server & inizialize configuration variables
+   require('./includes/config.inc.php');
+   
+
+	
+</SCRIPT>
+
+<HTML>
+<HEAD>
+<TITLE></TITLE>
+
+
+</HEAD>
+
+<BODY  BGCOLOR="#FFFFFF" TEXT="#08428C" LINK="#08428C" VLINK="#08428C" ALINK="#08428C">
+
+<SCRIPT Language=PHP>
+
+require("header.php");
+
+
+	print "<FONT FACE=\"Verdana,Helvetica,Arial\" SIZE=\"4\">";
+ 	print "<BR><CENTER><B>$MSG_164</B><BR><BR>$MSG_188</CENTER>";
+
+
+	print "<FONT FACE=\"Verdana,Helvetica,Arial\" SIZE=\"2\">";
+ 	print "<BR><CENTER><B>$MSG_186</B></CENTER>";
+
+</SCRIPT>
+
+<? require("./footer.php"); ?>
+</BODY>
+</HTML>

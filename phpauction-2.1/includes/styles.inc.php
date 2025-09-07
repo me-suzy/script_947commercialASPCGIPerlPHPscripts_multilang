870d1c9c1e946f5bde29626429d15b94802700af
me-suzy <me.suzana@gmail.com>
2025-09-07 08:50:55 +0300

    Add phpauction-2.1/includes/styles.inc.php

---
+++ phpauction-2.1/includes/styles.inc.php
@@ -0,0 +1,9 @@
+<STYLE type="text/css">
+<!--
+.links {  font: 10pt Verdana, Arial, Helvetica, sans-serif; color: #000066; text-decoration: none}
+.active {  font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 8pt; font-weight: bold; color: #FFFFFF; text-decoration: none}
+.inactive { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 8pt; font-weight: bold; color: #3A607A; text-decoration: none }
+.nounderlined { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 8pt; font-weight: bold; color: #3A607A; text-decoration: none }
+.navigation { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10pt; color: #000066;}
+-->
+</STYLE>

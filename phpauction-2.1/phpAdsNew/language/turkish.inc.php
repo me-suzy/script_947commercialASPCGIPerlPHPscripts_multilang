db22a1d7922a0977c9c01be1b7c2b4a6a73d3689
me-suzy <me.suzana@gmail.com>
2025-09-07 08:58:03 +0300

    Add phpauction-2.1/phpAdsNew/language/turkish.inc.php

---
+++ phpauction-2.1/phpAdsNew/language/turkish.inc.php
@@ -0,0 +1,329 @@
+<?php // $Revision: 1.1.1.1 $
+
+/************************************************************************/
+/* phpAdsNew 2                                                          */
+/* ===========                                                          */
+/*                                                                      */
+/* Copyright (c) 2001 by the phpAdsNew developers                       */
+/* http://sourceforge.net/projects/phpadsnew                            */
+/*                                                                      */
+/* Translations by Mesut Tunga - mesut@tunga.com                        */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+
+// Set translation strings
+$GLOBALS['strHome'] = "Yönetim Alaný";
+$GLOBALS['date_format'] = "%d/%m/%Y";
+$GLOBALS['time_format'] = "%H:%i:%S";
+$GLOBALS['strMySQLError'] = "MySQL-Hata:";
+$GLOBALS['strAdminstration'] = "Yönetim";
+$GLOBALS['strAddClient'] = "Yeni Müþteri Ekle";
+$GLOBALS['strModifyClient'] = "Müþteri Düzenle";
+$GLOBALS['strDeleteClient'] = "Müþteri Sil";
+$GLOBALS['strViewClientStats'] = "Müþteri Ýstatistikleri";
+$GLOBALS['strClientName'] = "Müþteri";
+$GLOBALS['strContact'] = "Ýletiþim";
+$GLOBALS['strEMail'] = "E-posta";
+$GLOBALS['strViews'] = "Reklam Gösterim";
+$GLOBALS['strClicks'] = "Reklam Týklanma";
+$GLOBALS['strTotalViews'] = "Toplam Görüntülenme";
+$GLOBALS['strTotalClicks'] = "Toplam Tyýlanma";
+$GLOBALS['strCTR'] = "Týklanma Oraný";
+$GLOBALS['strTotalClients'] = "Toplam Müþteri";
+$GLOBALS['strActiveClients'] = "Aktif Müþteriler";
+$GLOBALS['strActiveBanners'] = "Aktif Reklamlar";
+$GLOBALS['strLogout'] = "Çýkýþ";
+$GLOBALS['strCreditStats'] = "Reklam Bilgileri";
+$GLOBALS['strViewCredits'] = "Kalan Gösterim Sayýsý";
+$GLOBALS['strClickCredits'] = "Kalan Týklanma Sayýsý";
+$GLOBALS['strPrevious'] = "Önceki";
+$GLOBALS['strNext'] = "Sonraki";
+$GLOBALS['strNone'] = "Yok";
+$GLOBALS['strViewsPurchased'] = "Satýn Alýnan Görüntülenme Sayýsý";
+$GLOBALS['strClicksPurchased'] = "Satýn Aýynan Týklanma Sayýsý";
+$GLOBALS['strDaysPurchased'] = "Satýn Alýnan Gün Sayýsý";
+$GLOBALS['strHTML'] = "HTML";
+$GLOBALS['strAddSep'] = "Ya yukarýdaki alanlarý doldurunuz ya da aþaðýdaki alaný doldurunuz!";
+$GLOBALS['strTextBelow'] = "Reklam Altýndaki Yazý";
+$GLOBALS['strSubmit'] = "Kaydet";
+$GLOBALS['strUsername'] = "Kullanýcý Adý";
+$GLOBALS['strPassword'] = "Þifre";
+$GLOBALS['strBannerAdmin'] = "Reklam Yönetimi: ";
+$GLOBALS['strNoBanners'] = "Hiç reklam yok";
+$GLOBALS['strBanner'] = "Reklam";
+$GLOBALS['strCurrentBanner'] = "Geçerli Reklam";
+$GLOBALS['strDelete'] = "Sil";
+$GLOBALS['strAddBanner'] = "Yeni Reklam Ekle";
+$GLOBALS['strModifyBanner'] = "Reklam Düzenle";
+$GLOBALS['strURL'] = "Müþteri Adresi (http:// ekleyiniz)";
+$GLOBALS['strKeyword'] = "Anahtar Kelimeler";
+$GLOBALS['strWeight'] = "Aðýrlýk";
+$GLOBALS['strAlt'] = "Reklam Alt Yazýsý";
+$GLOBALS['strUsername'] = "Kullanýcý Adý";
+$GLOBALS['strPassword'] = "Þifre";
+$GLOBALS['strAccessDenied'] = "Eriþim yetkiniz yok!";
+$GLOBALS['strPasswordWrong'] = "Hataly Þifre";
+$GLOBALS['strNotAdmin'] = "Bu iþlem için yetkiniz yok";
+$GLOBALS['strClientAdded'] = "Müþteri Eklendi.";
+$GLOBALS['strClientModified'] = "Müþteri Bilgisi Düzenlendi.";
+$GLOBALS['strClientDeleted'] = "Müþteri Silindi.";
+$GLOBALS['strBannerAdmin'] = "Reklam Yönetimi";
+$GLOBALS['strBannerAdded'] = "Reklam Eklendi.";
+$GLOBALS['strBannerModified'] = "Reklam Düzenlendi.";
+$GLOBALS['strBannerDeleted'] = "Reklam Silindi";
+$GLOBALS['strBannerChanged'] = "Reklam Deðiþtirildi";
+$GLOBALS['strStats'] = "Ýstatistik";
+$GLOBALS['strDailyStats'] = "Günlük Ýstatistikler";
+$GLOBALS['strDetailStats'] = "Detayly Ýstatistik";
+$GLOBALS['strCreditStats'] = "Reklam Ýstatistikleri";
+$GLOBALS['strActive'] = "aktif";
+$GLOBALS['strActivate'] = "Aktif Et";
+$GLOBALS['strDeActivate'] = "Pasif Et";
+$GLOBALS['strAuthentification'] = "Yetkili Giriþi";
+$GLOBALS['strGo'] = "Git";
+$GLOBALS['strLinkedTo'] = "linked to";
+$GLOBALS['strBannerID'] = "Reklam-ID";
+$GLOBALS['strClientID'] = "Müþteri ID";
+$GLOBALS['strMailSubject'] = "Reklam Raporlarý";
+$GLOBALS['strMailSubjectDeleted'] = "Pasif Reklamlar";
+$GLOBALS['strMailHeader'] = "Sayin {contact},\n";
+$GLOBALS['strMailBannerStats'] = "{clientname} için reklam istatistikleri asagidaki gibidir:";
+$GLOBALS['strMailFooter'] = "Tesekkurler,\n   {adminfullname}";
+$GLOBALS['strLogMailSent'] = "[Reklamci] Istatistik Basari Ile Gonderildi.";
+$GLOBALS['strLogErrorClients'] = "[Reklamci] Musteriler db den alinirken bir hata olustu.";
+$GLOBALS['strLogErrorBanners'] = "[Reklamci] Reklamlar db den alinirken bir hata olustu.";
+$GLOBALS['strLogErrorViews'] = "[Reklamci] Reklam Goruntuleri db den alinirken bir hata olustu.";
+$GLOBALS['strLogErrorClicks'] = "[Reklamci] Reklam Tiklari db den alinirken bir hata olustu.";
+$GLOBALS['strLogErrorDisactivate'] = "[Reklamci] Reklami pasif ederken bir hata olustu.";
+$GLOBALS['strRatio'] = "Týklanma Orany";
+$GLOBALS['strChooseBanner'] = "Reklamý tipini seçiniz.";
+$GLOBALS['strMySQLBanner'] = "Reklamý Sql'e kaydet";
+$GLOBALS['strWebBanner'] = "Reklamý ftp ile gönder";
+$GLOBALS['strURLBanner'] = "Reklamý URL'den al";
+$GLOBALS['strHTMLBanner'] = "HTML Reklamý";
+$GLOBALS['strNewBannerFile'] = "Yeni Reklamý Dosyasý";
+$GLOBALS['strNewBannerURL'] = "Yeni Reklamý URL (http:// ekleyiniz)";
+$GLOBALS['strWidth'] = "Boy";
+$GLOBALS['strHeight'] = "En";
+$GLOBALS['strTotalViews7Days'] = "Son 7 Gün Ýçin Toplam Reklam Görüntülenme Sayýsý";
+$GLOBALS['strTotalClicks7Days'] = "Son 7 Gün Ýçin Toplam Reklam Görüntülenme Sayýsý";
+$GLOBALS['strAvgViews7Days'] = "Son 7 Gün Ýçin Ortalama Reklam Görüntülenme Sayýsý";
+$GLOBALS['strAvgClicks7Days'] = "Son 7 Gün Ýçin Ortalama Reklam Týklanma Sayýsý";
+$GLOBALS['strTopTenHosts'] = "En Çok Reklam Gören On Adres";
+$GLOBALS['strTopTenClickerHosts'] = "En Çok Reklam Týklayan On Adres";
+$GLOBALS['strClientIP'] = "Müþteri IP";
+$GLOBALS['strUserAgent'] = "Tarayycy regexp";
+$GLOBALS['strWeekDay'] = "Günler (0 - 6)";
+$GLOBALS['strDomain'] = "Alanadý (nokta dahil)";
+$GLOBALS['strSource'] = "Kaynak";
+$GLOBALS['strTime'] = "Saat";
+$GLOBALS['strAllow'] = ":zin Ver";
+$GLOBALS['strDeny'] = "Yok Say";
+$GLOBALS['strResetStats'] = "Ýstatistikleri Sýfýrla";
+$GLOBALS['strExpiration'] = "Bitiþ Tarihi";
+$GLOBALS['strNoExpiration'] = "Bitiþ Tarihi Yok";
+$GLOBALS['strDaysLeft'] = "Kalan Günler";
+$GLOBALS['strEstimated'] = "Tahmini Bitiþ Tarihi";
+$GLOBALS['strConfirm'] = "Eminmisiniz ?";
+$GLOBALS['strBannerNoStats'] = "Bu reklam için istatistik bulunmamaktadýr!";
+$GLOBALS['strWeek'] = "Hafta";
+$GLOBALS['strWeeklyStats'] = "Haftalýk Ýstatistikler";
+$GLOBALS['strWeekDay'] = "Günler";
+$GLOBALS['strDate'] = "Tarih";
+$GLOBALS['strCTRShort'] = "Oran";
+$GLOBALS['strDayShortCuts'] = array("Paz","Pzt","Sal","Çar","Per","Cum","Cts");
+$GLOBALS['strShowWeeks'] = "Hafta Sayýsý";
+$GLOBALS['strAll'] = "tümü";
+$GLOBALS['strAvg'] = "Ortalama.";
+$GLOBALS['strHourly'] = "Saatlik Görüntülenme/Týklanma";
+$GLOBALS['strTotal'] = "Toplam";
+$GLOBALS['strUnlimited'] = "Sýnýrsýz";
+$GLOBALS['strSave'] = "Kaydet";
+$GLOBALS['strUp'] = "Yukarý";
+$GLOBALS['strDown'] = "Aþaðý";
+$GLOBALS['strSaved'] = " kaydedildi!";
+$GLOBALS['strDeleted'] = "silindi!";
+$GLOBALS['strMovedUp'] = "üste alýndý";
+$GLOBALS['strMovedDown'] = "alta alyýdý";
+$GLOBALS['strUpdated'] = "güncellendi";
+$GLOBALS['strLogin'] = "Giriþ";
+$GLOBALS['strPreferences'] = "Özellikler";
+$GLOBALS['strAllowClientModifyInfo'] = "Müþteri bilgilerini düzenleyebilsin";
+$GLOBALS['strAllowClientModifyBanner'] = "Müþteri reklam düzenleyebilsin";
+$GLOBALS['strAllowClientAddBanner'] = "Müþteri yeni reklam girebilsin";
+$GLOBALS['strLanguage'] = "Dil";
+$GLOBALS['strDefault'] = "Default";
+$GLOBALS['strErrorViews'] = "Geçerli bir görüntüleme Sayýsý girin veya sýnýrsýz seçeneðini seçiniz !";
+$GLOBALS['strErrorNegViews'] = "Negatif Sayý giremezsiniz";
+$GLOBALS['strErrorClicks'] =  "Geçerli bir Týklanma Sayýsý girin veya sýnýrsýz seçeneðini seçiniz !";
+$GLOBALS['strErrorNegClicks'] = "Negatif Sayý giremezsiniz";
+$GLOBALS['strErrorDays'] = "Geçerli bir reklam görüntüleme gün Sayýsý girin veya sýnýrsýz seçeneðini seçiniz !";
+$GLOBALS['strErrorNegDays'] = "Negatif Sayý giremezsiniz";
+$GLOBALS['strTrackerImage'] = "Tracker Resmi:";
+
+// New strings for version 2
+$GLOBALS['strNavigation'] 				= "Menü";
+$GLOBALS['strShortcuts'] 				= "Kýsa Yollar";
+$GLOBALS['strDescription'] 				= "Açýklama";
+$GLOBALS['strClients'] 					= "Müþteriler";
+$GLOBALS['strID']				 		= "ID";
+$GLOBALS['strOverall'] 					= "Tümü";
+$GLOBALS['strTotalBanners'] 			= "Toplam Reklam";
+$GLOBALS['strToday'] 					= "Bu Gün";
+$GLOBALS['strThisWeek'] 				= "Bu Hafta";
+$GLOBALS['strThisMonth'] 				= "Bu Ay";
+$GLOBALS['strBasicInformation'] 		= "Temel Bilgiler";
+$GLOBALS['strContractInformation'] 		= "Eriþim Bilgileri";
+$GLOBALS['strLoginInformation'] 		= "Giriþ Bilgileri";
+$GLOBALS['strPermissions'] 				= "Yetkiler";
+$GLOBALS['strGeneralSettings']			= "Genel Ayarlar";
+$GLOBALS['strSaveChanges']		 		= "Kaydet";
+$GLOBALS['strCompact']					= "Az Detaylý";
+$GLOBALS['strVerbose']					= "Çok Detaylý";
+$GLOBALS['strOrderBy']					= "sýralama";
+$GLOBALS['strShowAllBanners']	 		= "Tüm reklamlarý göster";
+$GLOBALS['strShowBannersNoAdClicks']	= "Görüntülenme Sayýlý reklamlarý göster";
+$GLOBALS['strShowBannersNoAdViews']		= "Týklanma Sayýlý reklamlarý göster";
+$GLOBALS['strShowAllClients'] 			= "Tüm Müþterileri göster";
+$GLOBALS['strShowClientsActive'] 		= "aktif reklamlarý olan Müþterileri göster";
+$GLOBALS['strShowClientsInactive']		= "Pasif reklamlarý olan Müþterileri göster";
+$GLOBALS['strSize']						= "Boyut";
+
+$GLOBALS['strMonth'] 					= array("Ocak","Þubat","Mart","Nisan","Mayýs","Haziran","Temmuz", "Aðustos", "Eylül", "Ekim", "Kasým", "Aralýk");
+$GLOBALS['strDontExpire']				= "Bitiþ Tarihi Yok";
+$GLOBALS['strActivateNow'] 				= "Bu kampanyayý hemen baþlat";
+$GLOBALS['strExpirationDate']			= "Bitiþ Tarihi";
+$GLOBALS['strActivationDate']			= "Baþlangýç Tarihi";
+
+$GLOBALS['strMailClientDeactivated'] 	= "Aþaðýdaki reklamlar pasif edildi. Çünkü: ";
+$GLOBALS['strMailNothingLeft'] 			= "Reklaminizi yenilemek isterseniz lutfen bizimle irtibata geciniz.\n\nSizinle calismak bizim icin zevkti.";
+$GLOBALS['strClientDeactivated']		= "Bu kampanya su an pasif durumda. Çünkü: ";
+$GLOBALS['strBeforeActivate']			= "Aktivasyon tarihi henüz gelmedi.";
+$GLOBALS['strAfterExpire']				= "Reklam Gosterim Suresi Doldu.";
+$GLOBALS['strNoMoreClicks']				= "Satin alinan Tiklanma Sayisi doldu.";
+$GLOBALS['strNoMoreViews']				= "Satin alinan Goruntulenme Sayisi doldu.";
+
+$GLOBALS['strBanners'] 					= "Reklamlar";
+$GLOBALS['strCampaigns']				= "Kampanyalar";
+$GLOBALS['strCampaign']					= "Kampanya";
+$GLOBALS['strCampaignInfo']				= "Kampanya Bilgileri";
+$GLOBALS['strModifyCampaign']			= "Kampanya Düzenle";
+$GLOBALS['strName']						= "Kampanya Adý";
+$GLOBALS['strBannersWithoutCampaign']	= "Kampanyasýz Reklamlar";
+$GLOBALS['strMoveToNewCampaign']		= "Yeni Kampanyaya Taþý";
+$GLOBALS['strCreateNewCampaign']		= "Yeni Kampanya Oluþtur";
+$GLOBALS['strEditCampaign']				= "Kampanya Düzenle";
+$GLOBALS['strEdit']						= "Düzenle";
+$GLOBALS['strCreate']					= "Kampanya Oluþtur";
+$GLOBALS['strUntitled']					= "Adsýz";
+
+$GLOBALS['strTotalCampaigns'] 			= "Toplam Kampanyalar";
+$GLOBALS['strActiveCampaigns'] 			= "Aktif Kampanyalar";
+
+$GLOBALS['strLinkedTo']					= "url si";
+$GLOBALS['strNoViewLoggedInInterval']   = "Son {interval} günde görüntülenme yok";
+$GLOBALS['strNoClickLoggedInInterval']  = "Son {interval} günde Týklanma yok";
+$GLOBALS['strSendAdvertisingReport']	= "Eposta ile reklam raporu gönder";
+$GLOBALS['strNoDaysBetweenReports']		= "Reklam raporlarý arasý gün Sayýsý";
+$GLOBALS['strSendDeactivationWarning']  = "Bu kampanya bittiðinde uyarý gönder";
+
+$GLOBALS['strWarnClientTxt']			= "Reklamýnýza ait görüntülenme veya týklanma sayýsý {limit} den daha az kaldý. \nReklamýnýz çok yakýnda pasif edilecek. ";
+$GLOBALS['strViewsClicksLow']			= "Reklam görüntülenme/Týklanma Sayýsý düþük";
+
+$GLOBALS['strDays']						= "Gün";
+$GLOBALS['strHistory']					= "Geçmiþ";
+$GLOBALS['strAverage']					= "Ortalama";
+$GLOBALS['strDuplicateClientName']		= "Girdiðiniz kullanycý adý sistemde kayýtlý, lütfen farklý bir isim deneyiniz.";
+$GLOBALS['strAllowClientDisableBanner'] = "Müþteri reklamlarýný pasif edebilsin";
+$GLOBALS['strAllowClientActivateBanner'] = "Müþteri reklamlarýný aktif edebilsin";
+
+$GLOBALS['strGenerateBannercode']		= "Reklam Kodunu Oluþtur";
+$GLOBALS['strChooseInvocationType']		= "Kod oluþturma þeklini seçiniz";
+$GLOBALS['strGenerate']					= "Oluþtur";
+$GLOBALS['strParameters']				= "Paremetreler";
+$GLOBALS['strUniqueidentifier']			= "Tekil kimlik";
+$GLOBALS['strFrameSize']				= "Frame boyutu";
+$GLOBALS['strBannercode']				= "Reklam Kodu";
+
+$GLOBALS['strSearch']					= "Ara";
+$GLOBALS['strNoMatchesFound']			= "Aramanýza uyan döküman bulunamadý";
+
+$GLOBALS['strNoViewLoggedInInterval']   = "Verilen tarih aralýðýna göre bu raporda görüntülenme bulunmamaktadýr.";
+$GLOBALS['strNoClickLoggedInInterval']  = "Verilen tarih aralýðýna göre bu raporda týklanma bulunmamaktadýr";
+$GLOBALS['strMailReportPeriod']			= "Bu rapor {startdate} - {enddate} tarihleri arasý istatistikleri içermektedir.";
+$GLOBALS['strMailReportPeriodAll']		= "Bu rapor {enddate} tarihine kadar tüm istatistikleri içermektedir.";
+$GLOBALS['strNoStatsForCampaign'] 		= "Bu kampanya için istatistik bulunmamaktadýr";
+$GLOBALS['strFrom']						= "Kimden";
+$GLOBALS['strTo']						= "kime";
+$GLOBALS['strMaintenance']				= "Bakým";
+$GLOBALS['strCampaignStats']			= "Kampanya istatistikleri";
+$GLOBALS['strClientStats']				= "Müþteri istatistikleri";
+$GLOBALS['strErrorOccurred']			= "Bir hata oluþtu";
+$GLOBALS['strAdReportSent']				= "Reklam raporlarý gönderildi";
+
+$GLOBALS['strAutoChangeHTML']			= "Reklam týklanmalarýný loglamak için HTML kodunu deðiþtir";
+
+$GLOBALS['strZones']					= "Bölgeler";
+$GLOBALS['strAddZone']					= "Bölge oluþtur";
+$GLOBALS['strModifyZone']				= "Bölge düzenle";
+$GLOBALS['strAddNewZone']				= "Yeni bölge ekle";
+
+$GLOBALS['strOverview']					= "Genel Bakýþ";
+$GLOBALS['strEqualTo']					= "is equal to";
+$GLOBALS['strDifferentFrom']			= "is different from";
+$GLOBALS['strAND']						= "VE";  // logical operator
+$GLOBALS['strOR']						= "VEYA"; // logical operator
+$GLOBALS['strOnlyDisplayWhen']			= "Bu reklamý þu durumlarda göster:";
+
+$GLOBALS['strStatusText']				= "Durum";
+
+$GLOBALS['strConfirmDeleteClient'] 		= "Bu müþteriyi silmek istediðinizden eminmisiniz?";
+$GLOBALS['strConfirmDeleteCampaign']	= "Bu kampanyayý silmek istediðinizden eminmisiniz?";
+$GLOBALS['strConfirmDeleteBanner']		= "Bu reklamý silmek istediðinizden eminmisiniz?";
+$GLOBALS['strConfirmResetStats']		= "Tüm istatistikleri silmek istediðinizden eminmisiniz?";
+$GLOBALS['strConfirmResetCampaignStats']= "Bu kampanya için istatistikleri silmek istediðinizden eminmisiniz?";
+$GLOBALS['strConfirmResetClientStats']	= "Bu müþteri için istatistikleri silmek istediðinizden eminmisiniz?";
+$GLOBALS['strConfirmResetBannerStats']	= "Bu reklam için istatistikleri silmek istediðinizden eminmisiniz?";
+
+$GLOBALS['strClientsAndCampaigns']		= "Müþteriler & Kampanyalar";
+$GLOBALS['strCampaignOverview']			= "Kampanya Genel Bakýþ";
+$GLOBALS['strReports']					= "Raporlar";
+$GLOBALS['strShowBanner']				= "Reklamý Göster";
+
+$GLOBALS['strIncludedBanners']			= "Baðlý reklamlar";
+$GLOBALS['strProbability']				= "Ýhtimal";
+$GLOBALS['strInvocationcode']			= "Invocationcode";
+$GLOBALS['strSelectZoneType']			= "Lütfen baðlý reklam tipini seçiniz";
+$GLOBALS['strBannerSelection']			= "Reklam seçimi";
+$GLOBALS['strInteractive']				= "Dinamik";
+$GLOBALS['strRawQueryString']			= "Raw querystring";
+
+$GLOBALS['strBannerWeight']				= "Reklam Aðýrlýðý";
+$GLOBALS['strCampaignWeight']			= "Kampanya Aðýrlýðý";
+
+$GLOBALS['strZoneCacheOn']				= "Bölge kaydetme açýk";
+$GLOBALS['strZoneCacheOff']				= "Bölge kaydetme kapalý";
+$GLOBALS['strCachedZones']				= "Kaydedilen Bölgeler";
+$GLOBALS['strSizeOfCache']				= "Kaydetme Boyutu";
+$GLOBALS['strAverageAge']				= "Ortalama";
+$GLOBALS['strRebuildZoneCache']			= "Bölge Kayýtlarýný Yenile";
+$GLOBALS['strKiloByte']					= "KB";
+$GLOBALS['strSeconds']					= "saniye";
+$GLOBALS['strExpired']					= "Bitti";
+
+$GLOBALS['strModifyBannerAcl'] 			= "Display limitations";
+$GLOBALS['strACL'] 						= "Limit";
+$GLOBALS['strNoMoveUp'] 				= "Ýlk satýrý üste alamazsýnýz";
+$GLOBALS['strACLAdd'] 					= "Add new limitation";
+$GLOBALS['strNoLimitations']			= "No limitations";
+
+$GLOBALS['strLinkedZones']				= "Linked Zones";
+$GLOBALS['strNoZonesToLink']			= "There are no zones available to which this banner can be linked";
+
+$GLOBALS['strCustom']					= "Custom";
+
+?>

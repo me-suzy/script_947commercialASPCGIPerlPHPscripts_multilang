2a71725960f988f3bfcf39f0cd95b1cb2b4ab681
me-suzy <me.suzana@gmail.com>
2025-09-07 08:48:12 +0300

    Add phpauction-2.1/admin/loggedin.inc.php

---
+++ phpauction-2.1/admin/loggedin.inc.php
@@ -0,0 +1,31 @@
+<?
+
+/*
+
+   Copyright (c), 1999, 2000, 2001 - phpauction.org                  
+   
+   This program is free software; you can redistribute it and/or modify 
+   it under the terms of the GNU General Public License as published by 
+   the Free Software Foundation (version 2 or later).                                  
+                                                                        
+   This program is distributed in the hope that it will be useful,      
+   but WITHOUT ANY WARRANTY; without even the implied warranty of       
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+   GNU General Public License for more details.                         
+                                                                        
+   You should have received a copy of the GNU General Public License    
+   along with this program; if not, write to the Free Software          
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   
+*/
+
+	#// Check if admin user is logged in
+	session_name("PHPAUCTIONADMIN");
+	session_start();
+	
+	if(empty($HTTP_SESSION_VARS[PHPAUCTION_ADMIN_LOGIN]))
+	{
+		Header("Location: login.php");
+		exit;
+	}
+?>

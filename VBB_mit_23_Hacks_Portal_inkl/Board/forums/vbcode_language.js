b4813b1608c3b7a3af9d1c415a3bd1f1eaefa8e0
me-suzy <me.suzana@gmail.com>
2025-09-07 09:02:48 +0300

    Add VBB_mit_23_Hacks_Portal_inkl/Board/forums/vbcode_language.js

---
+++ VBB_mit_23_Hacks_Portal_inkl/Board/forums/vbcode_language.js
@@ -0,0 +1,62 @@
+// the following are the text prompts for buttons etc.
+// DO NOT ADD LINE-BREAKS BETWEEN THE "...." QUOTES!
+
+// MINI-HELP MESSAGES
+
+b_text = "Insert BOLD text";
+i_text = "Insert ITALIC text";
+u_text = "Insert UNDERLINED text";
+
+size_text = "Alter the SIZE of your text";
+font_text = "Alter the FONT of your text";
+color_text = "Alter the COLOR of your text";
+
+url_text = "Insert a hyperlink into your message";
+email_text = "Insert an email-link into your message";
+img_text = "Insert an image into your message";
+
+code_text = "Insert monospace text into your message";
+php_text = "Insert text with PHP syntax highlighting";
+list_text = "Insert an ordered list into your message";
+quote_text = "Insert a quote into your message";
+
+norm_text = "Switch to normal code-editing mode";
+enha_text = "Enhanced mode: allows complex nesting of tags";
+
+closecurrent_text = "Close the current vBcode tag";
+closeall_text = "Close all open vBcode tags";
+
+align_text = "Text-Ausrichtung ändern";
+code_text = "Monospace Text einfügen";
+move_text = "Laufschrift einfügen";
+tt_text = "Schreibmaschinenschrift einfügen";
+cursor_text = "Cursor verändern";
+nimg_text = "Foto-Negativ-Effekt Graustufen";
+wt_text = "Wellenartige Störung";
+s_text = "Schatten";
+mark_text = "Markieren";
+glow_text = "Glühender Text";
+bt_text = "Blur Text";
+d_text = "Text durchstreichen";
+sub_text = "Tiefgestellter Text";
+sup_text = "Hochgestellter Text";
+blink_text = "Blinkender Text >>>NUR Netscape<<<";
+ftp_text = "Link zum FTP mit Login,Port,Verzeichniss";
+// ERROR MESSAGES
+
+enhanced_only_text = "<< This is only available in Enhanced Mode >>";
+no_tags_text = "<< No open vBcode tags detected. >>";
+already_open_text = "<< You already have an open tag of this type >>";
+
+// TEXT FOR POP-UP PROMPTS
+
+tag_prompt = "Enter the text to be formatted:";
+
+font_formatter_prompt = "Enter the text to be formatted with the specified";
+
+link_text_prompt = "Enter the text to be displayed for the link (optional)";
+link_url_prompt = "Enter the full URL for the link";
+link_email_prompt = "Enter the email address for the link";
+
+list_type_prompt = "What type of list do you want? Enter '1' for a numbered list, enter 'a' for an alphabetical list, or leave blank for a list with bullet points.";
+list_item_prompt = "Enter a list item.\nLeave the box empty or press 'Cancel' to complete the list.";

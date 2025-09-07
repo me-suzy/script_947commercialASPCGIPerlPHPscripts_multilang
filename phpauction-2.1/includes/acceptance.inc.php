ff9c451ba6747fa568ddffaa01fe50a13d29f016
me-suzy <me.suzana@gmail.com>
2025-09-07 08:49:59 +0300

    Add phpauction-2.1/includes/acceptance.inc.php

---
+++ phpauction-2.1/includes/acceptance.inc.php
@@ -0,0 +1,12 @@
+<? print $sml_font; ?>
+<!-- Add the acceptance message (in HTML) you wan to appear at the bottom of 
+     the users registration form, before the submit bottom.
+     If you don't want any text to appear there, leave this document empty 
+     or (reccomended) set the $SHOW_ACCEPTANCE_TEXT varial in
+     includes/config.inc.php to zero
+-->
+By submitting this form and sending your data you explicitly accept
+the terms and conditions regulating the use of this web site.
+<BR>
+
+

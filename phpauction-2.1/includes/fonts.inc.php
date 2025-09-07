840e8140cc85e073fdf2e9ccee612cb9f870c742
me-suzy <me.suzana@gmail.com>
2025-09-07 08:50:31 +0300

    Add phpauction-2.1/includes/fonts.inc.php

---
+++ phpauction-2.1/includes/fonts.inc.php
@@ -0,0 +1,7 @@
+<?
+	$FONTS = array("1" => "Verdana,Arial,Helvetica",
+				   "2" => "Tahoma,Verdana,Arial",
+				   "3" => "Times New Roman,Times,serif",
+				   "4" => "Courier,Courier New"
+				  );
+?>

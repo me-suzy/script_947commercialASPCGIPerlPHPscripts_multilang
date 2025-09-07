43f048c3083c8e188a4855dededc7a607051e74e
me-suzy <me.suzana@gmail.com>
2025-09-07 09:05:32 +0300

    Add VBB_mit_23_Hacks_Portal_inkl/Board/includes/center/phpscripts.php

---
+++ VBB_mit_23_Hacks_Portal_inkl/Board/includes/center/phpscripts.php
@@ -0,0 +1,30 @@
+<?php
+$centerblocks_modules[php] = array(
+	'title' => 'PHP Script',
+	'func_display' => 'centerblocks_php_block',
+	'func_add' => '',
+	'func_update' => '',
+	'text_type' => 'PHP',
+	'text_type_long' => 'PHP Script',
+	'text_content' => 'PHP Script',
+	'support_nukecode' => false,
+	'allow_create' => true,
+	'allow_delete' => true,
+	'form_url' => true,
+	'form_content' => true,
+	'form_refresh' => false,
+	'show_preview' => true,
+    'has_templates' => false
+    );
+
+function centerblocks_php_block($row) {
+	global $nuke_content;
+	ob_start();
+	print eval($row[content]);
+	$row[content] = ob_get_contents();
+	ob_end_clean();
+	$block_title = $row[title];
+    $block_content = $row[content];
+    eval("dooutput(\"".gettemplate('P_themecenterbox')."\");");
+}
+?>
